10bcadb1fee6012835ba90e142e0389c08899088
ortizgmariana <A01610012@itesm.mx>
2016-03-19 00:45:54 -0600

    Create password_form.php

---
+++ password_form.php
@@ -0,0 +1,22 @@
+<form action="password.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="oldpassword" placeholder="Old Password" type="password"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="newpassword" placeholder="New Password" type="password"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="confirmation" placeholder="New Password Confirmation" type="password"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+                Change password
+            </button>
+        </div>
+    </fieldset>
+</form>
+<div>
+    
+</div>
