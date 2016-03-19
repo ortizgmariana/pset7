4bba047a5ff1575422145760310bde61c931bdc5
ortizgmariana <A01610012@itesm.mx>
2016-03-19 00:38:32 -0600

    Create quote_form.php

---
+++ quote_form.php
@@ -0,0 +1,12 @@
+<form action = "quote.php" method = "post">
+    <fieldset>
+    <div class = "form-group">
+        <input class ="form-control" name = "symbol" placeholder= "Symbol" type:"text"/>
+    </div>
+    <div class= "form-group">
+        <button  class="btn btn-default" type="submit">
+         Search quote
+         </button>
+        </div>
+    </fieldset>
+</form>
