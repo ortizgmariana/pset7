c7ff08f9e8311c91447deeefd32955f0e29e8e1d
ortizgmariana <A01610012@itesm.mx>
2016-03-19 00:41:40 -0600

    Create buy_form.php

---
+++ buy_form.php
@@ -0,0 +1,13 @@
+<form action = "buy.php" method = "post">
+    <fieldset>
+        <div class = "form-group">
+            <input autofocus name="symbol" placeholder="Stock symbol" type="text"/>
+        </div> 
+        <div class="form-group">
+            <input name="shares" placeholder="shares" type="text"/>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn">Buy</button>
+        </div>
+    </fieldset>
+</form>
